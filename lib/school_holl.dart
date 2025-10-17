dd1b2460b04dad35395a208b5d0bcd3cc9b2819e
Pankaj841219 <pankaj.edugaon@gmail.com>
2025-10-17 15:55:17 +0530

    i create schoolHoll

---
+++ lib/school_holl.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SchoolHoll extends StatefulWidget {
+  const SchoolHoll({super.key});
+
+  @override
+  State<SchoolHoll> createState() => _SchoolHollState();
+}
+
+class _SchoolHollState extends State<SchoolHoll> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
